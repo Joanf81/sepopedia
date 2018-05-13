c6307539c50ff3b7111a315693db57fc94f6ed30
joan <joanf81@gmail.com>
2018-05-13 20:16:19 +0200

    Added configuration form devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,10 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # For devise:
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
