7eaeb7e500345a4fef5f365cdd8b999fa4932ef4
joan <joanf81@gmail.com>
2018-05-14 20:16:21 +0200

    Instaladdo FileUploader

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'devise/user_registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # FileUploader:
+  mount Uploader::Engine => '/uploader'
+
   root to: "home#index"
 end
