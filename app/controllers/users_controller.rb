fd410b663337efcd3ec1574403e2a7464dfda56f
joan <joanf81@gmail.com>
2018-06-24 12:29:27 +0200

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	skip_before_action :authenticate_user!, :only => :show
+
+	def my_profile
+		@resource = current_user
+		render :show
+	end
+
+	private
+
+	def resource_type
+		User
+	end
+
+	def resource_not_found_msg
+		"El usuario que intentas buscar no existe, intentelo de nuevo."
+	end
+
+	def decorate_resource?
+		true
+	end
+end
