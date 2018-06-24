7d92b0cb7489f664174da236afe1277ec2504a8a
joan <joanf81@gmail.com>
2018-06-24 12:25:09 +0200

    add locals

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'devise/user_registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # FileUploader:
-  mount Uploader::Engine => '/uploader'
+  devise_for :users, :controllers => { registrations: 'devise/user_registrations' }
 
   root to: "home#index"
+
+  resources :users, only: [:show] do
+  	get "my_profile" => "users#my_profile", on: :collection
+  end
 end
