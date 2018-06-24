7a28aa5b33b9c3634e17ba03f4a227f9f3e23351
joan <joanf81@gmail.com>
2018-06-24 12:25:40 +0200

    migrations

--- db/migrate/20180514180117_add_attributes_to_user.rb
+++ db/migrate/20180514180117_add_attributes_to_user.rb
@@ -1,6 +1,7 @@
 class AddAttributesToUser < ActiveRecord::Migration[5.2]
   def change
   	change_table :users do |t|
+  	  t.string :user_name
 	  t.string :first_name
 	  t.string :second_name
 	  t.string :tlf
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_181534) do
+ActiveRecord::Schema.define(version: 2018_05_31_153824) do
 
   create_table "assets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_181534) do
     t.string "second_name"
     t.string "tlf"
     t.text "content"
+    t.string "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
